88f2c51a2e0864062240bd599d09a81d9e8ebcdd
Arik Ahmed Zelan <info.zelankathi2001@gmail.com>
2023-08-29 02:11:43 +0600

    footer aded

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div
       className=" grid grid-cols-1 md:grid-cols-4 gap-y-10 
-      px-32 py-14 bg-gray-100
+      px-32 p bg-gray-100
       text-gray-600"
     >
       <div className="space-y-4 text-xs text-gray-800">
